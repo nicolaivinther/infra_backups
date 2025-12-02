6d54d811fd480249c93f486333a4ff6f05c5d093
Nicolai Tanghøj <nicolai92@gmail.com>
2025-12-02 21:55:02 +0100

    Add wireguard up and down per run

--- local_cronjobs/backup_postgres.sh
+++ local_cronjobs/backup_postgres.sh
@@ -9,4 +9,14 @@ set -a
 [ -f .development.env ] && source .development.env
 set +a
 
-./dump_psql_backup.sh
+# Start WireGuard
+sudo wg-quick up Nicolai_MacMini
+
+# Run backup (ensure WireGuard is stopped even if backup fails)
+./dump_psql_backup.sh || BACKUP_FAILED=1
+
+# Stop WireGuard
+sudo wg-quick down Nicolai_MacMini
+
+# Exit with backup status
+exit ${BACKUP_FAILED:-0}
